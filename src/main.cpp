950a0ef226a1e595409c1048345ea479c81e569b
DomAlcoholico <143230084+DomAlcoholico@users.noreply.github.com>
2023-09-02 01:04:54 +0000

    clase viernes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
-
+#include <iostream>
 int main(int argc, char const *argv[])
 {
-    for (size_t i = 0; i < count; i++)
+    std::cout<<"Mensaje"<<std::endl; 
+    int a = 2;
+    for (int nombre = 0; nombre < 10; nombre++)
     {
         /* code */
     }
-    
+
     return 0;
 }
